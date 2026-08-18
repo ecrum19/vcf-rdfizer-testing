072121ce16a60be720fc03844e12e0253c6c0976
ecrum19 <elicrum88@gmail.com>
2026-08-18 12:34:18 +0200

    fixed testing command 2

--- test_hdt_partitioned.sh
+++ test_hdt_partitioned.sh
@@ -2,15 +2,15 @@
 set -euo pipefail
 
 # Edit these paths for the test you want to run.
-INPUT_VCF="../vcf-rdfizer-testing/vcf_data/0GOOR_HG002.vcf.gz"
-OUTPUT_DIR="../vcf-rdfizer-testing/testing-results"
+INPUT_VCF="vcf-rdfizer-testing/vcf_data/0GOOR_HG002.vcf.gz"
+OUTPUT_DIR="vcf-rdfizer-testing/testing-results"
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd -- "$SCRIPT_DIR/.." && pwd)"
 
 cd "$PROJECT_ROOT"
 
-python3 ../VCF-RDFizer/vcf_rdfizer.py \
+python3 VCF-RDFizer/vcf_rdfizer.py \
   --mode full \
   --input "$INPUT_VCF" \
   --rdf-layout batch \
